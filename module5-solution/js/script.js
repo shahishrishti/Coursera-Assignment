f93b7308dcd16f718c44d73a3b3d853471f26b5b
Shrishti Shahi <shahishrishti@email.com>
2020-08-13 12:15:58 +0530

    Solution for module 5

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -74,14 +74,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // and process them: choose random category, retrieve home HTML snippet, insert that
 // random category into the home HTML snippet, and then insert that snippet into our
 // main page (index.html).
-showLoading("#main-content");
-$ajaxUtils.sendGetRequest(
-  homeHtmlUrl,
-  function(responseText){
-    document.querySelector("#main-content")
-      .innerHTML=responseText;
-  },
-  false);
 
 // TODO: STEP 1: Substitute [...] below with the *value* of the function buildAndShowHomeHTML,
 // so it can be called when server responds with the categories data.
